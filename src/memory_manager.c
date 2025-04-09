3070320d9d7949cb2f2d957903f7a80a98b67e89
Gustavo Prezoto <gustavoprezotoboca@gmail.com>
2025-04-08 21:18:59 -0300

    Adding comments to improve legibility.

--- src/memory_manager.c
+++ src/memory_manager.c
@@ -153,29 +153,39 @@ static void __mm_add_new_family_group_page_to_global_group(vm_page_for_families_
     last_family_group_page = new_page;
 }
 
+/**
+ * @brief This internal function is used for adding a new struct to a metadata
+ * family page. 
+ * 
+ * Iterates to find the next available family metadata group to add the new struct.
+ * It creates a new family metadata page if all pages are fully occupied.
+ * 
+ */
 static void __mm_add_struct_to_global_families(char *struct_name, __uint32_t struct_size) {    
     // Iterates to find the next available family metadata group to add the new struct.
+    // It creates a new family metadata page if all pages are occupied.
     vm_page_family_t *page_family_iter = NULL;
     vm_page_for_families_t *metadata_page_iter = first_family_group_page;
     while (TRUE) {
+        // Iterates the metadata_page current to find if there is a available spot in it.
         ITERATE_PAGE_FAMILIES_BEGIN(metadata_page_iter, page_family_iter) {
             if (page_family_iter->struct_size == NULL || page_family_iter->struct_size == 0) {
                 strncpy(page_family_iter->struct_name, struct_name, sizeof(page_family_iter->struct_name));
                 page_family_iter->struct_size = struct_size;
-                printf("Struct: %s\n", page_family_iter->struct_name);
                 return;
             }
         } ITERATE_PAGE_FAMILIES_END()
 
-        if (first_family_group_page->next == NULL) {
+        // If it checked all metadata pages and there is no empty spot for it, 
+        // then it creates a new metadata page.
+        if (metadata_page_iter->next == NULL) {
             vm_page_for_families_t *new_family_metadata_page = __mm_create_new_metadata_page_for_struct_families(struct_name, struct_size);
             new_family_metadata_page->next = first_family_group_page;
             first_family_group_page = new_family_metadata_page;
-            printf("Criou uma nova struct: %s\n", first_family_group_page->vm_page_family[0].struct_name);
             return;
         }
+        // If there is still pages to be iterated, switch iter to it.
         else {
-            printf("Procurando a proxima pagina\n");
             metadata_page_iter = first_family_group_page->next;
         }
     }
